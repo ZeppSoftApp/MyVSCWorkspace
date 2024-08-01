c7d2ce08d14fbdeb5e2ff4788fe1ca8d4788c204
Zepp <zeppsoftapp@gmail.com>
2024-08-01 11:44:37 +0300

    Init

---
+++ SimpleTest4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleTest4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter a name:");
+        string name = string.Empty;
+        name = Console.ReadLine();
+        System.Console.WriteLine($"Hello, {name} in SimpleTest4Ze!");
+    }
+}
